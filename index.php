1cc7f4e8c79f69f42832af564e2ee17c18c9e277
yochida <yochida2015@gmail.com>
2020-02-14 15:11:34 +0700

    change

--- index.php
+++ index.php
@@ -20,12 +20,18 @@ if (!is_null($events['events'])) {
             // Get replyToken
             $replyToken = $event['replyToken'];
 
+            if($event['message']['text'] == 'ok'){
+                $ms = 'ok';
+            }else{
+                $ms = 'no';
+            }
+
             $access_token = '3gRRrihoNlySOopovVwKXAqLMkHzXzslmg98EjkB1On4rvQ98mzJ4KQba60NVYcFX6YPDcFjByaKpiGNEspEJ2Dqm/KBtqi0Gl/LLAN0ILDerkzEGqwKEuhvEXh5lGqjQxiEKor9Ic+uumDquN/jWwdB04t89/1O/w1cDnyilFU=';
             $channelSecret = 'e97b555217c97f5889d0bfd50c6f049e';
             $idPush = $text;
             $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
             $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
-            $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($events);
+            $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($ms);
             $response = $bot->pushMessage($idPush, $textMessageBuilder);
 
             echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
